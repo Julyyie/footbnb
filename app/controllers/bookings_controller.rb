e4adc115049c555f87533cb49fecdd164fd5a071
mehdi sabil <mehdi.sabil@hotmail.com>
2023-03-01 14:40:42 +0100

    BookingControllerForm

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,23 @@
 class BookingsController < ApplicationController
 
   def new
-    @bookings = Booking.all
-
+    @booking = Booking.new
   end
 
   def create
     @player = Player.find(params[:player_id])
     @booking = Booking.new(booking_params)
-    # @booking.player = @player
-
+    @booking.player = @player
+    @booking.user = current_user
     if @booking.save
-      redirect_to booking_path
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    #if status decline booking.destroy status pending en db parce que pas encore accépté
+    #if status decline booking.destroy status pending en db parce que pas encore accepté
   end
 
   private
@@ -26,5 +25,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:rent_start, :rent_ending)
   end
-
 end
