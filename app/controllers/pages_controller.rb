0f7e085b5dc7e65c65f421f1f44c5b08150b84cc
mehdi sabil <mehdi.sabil@hotmail.com>
2023-03-01 17:58:22 +0100

    Dashboard_back

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,7 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-
-
-    # ceux que je mets en location
-    @my_players = @current_user.players
-
+    @my_players = current_user.players
+    @my_bookings = current_user.bookings
   end
-
 end
