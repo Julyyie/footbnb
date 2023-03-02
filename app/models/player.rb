397e567ce28ed907cc56ddbd5850db1f92a8f902
juliedelarue <delaruejuulie@gmail.com>
2023-03-02 17:56:55 +0100

    searchbar

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,16 +6,4 @@ class Player < ApplicationRecord
   validates :club, presence: true
 
   # validates :user_id, presence: true, through: :user
-
-  def display_position
-    {
-      "Left Back" => "Arrière gauche",
-      "Defensive Midfielder" => "Milieu défensif",
-      "Full Back" => "Défenseur central",
-      "Centre Forward" => "Attaquant de pointe",
-      "Midfielder" => "Milieu de terrain",
-      "Second Striker" => "Attaquant",
-      "Goalkeeper" => "Gardien de but"
-    }[position]
-  end
 end
