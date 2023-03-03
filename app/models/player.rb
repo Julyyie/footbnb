8b13db77e27d48fd26a31593ae3bbcf6918f8c0e
edouardguille <edouard.guille1@gmail.com>
2023-03-03 14:44:47 +0100

    front create player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,13 @@
 class Player < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
   validates :club, presence: true
 
-  scope :filter_by_status, -> (status) { where status: status }
-  scope :filter_by_location, -> (location_id) { where location_id: location_id }
-  scope :filter_by_starts_with, -> (name) { where("name like ?", "#{name}%")}
+  #scope :filter_by_status, ->(status) { where status: status }
+  #scope :filter_by_location, ->(location_id) { where location_id: location_id }
+  #scope :filter_by_starts_with, ->(name) { where("name like ?", "#{name}%") }
   # validates :user_id, presence: true, through: :user
 
 end
